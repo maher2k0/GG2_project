bb18017c9e80c43eacd4aeed48b0cc955e17728d
Maher Uddin <mu266@cam.ac.uk>
2022-05-19 16:13:44 +0100

    just added stuff that got overwritten

--- gg2_python/ct_test_example.py
+++ gg2_python/ct_test_example.py
@@ -18,7 +18,9 @@ source = Source()
 # all the output should be saved in a 'results' directory
 
 def test_1():
-	# explain what this test is for
+	# creates a phantom and uses the scan_and_reconstruct function to
+	# simulate CT scan and reconstruct image from sinogram.
+	# Testing reconstructed geometry
 
 	# work out what the initial conditions should be
 	p = ct_phantom(material.name, 256, 3)
@@ -29,7 +31,12 @@ def test_1():
 	save_draw(y, 'results', 'test_1_image')
 	save_draw(p, 'results', 'test_1_phantom')
 
-	# how to check whether these results are actually correct?
+	# To check if results are correct, visually inspect the phantom and reconstructed image
+	# It's expected that the two images have the same geometry
+	# They will however have different data levels since the phantom is constructed
+	# from a list of indeces related to the material type whereas the reconstructed
+	# image is constructed from a list of linear attenuations
+
 
 def resolution_real():
 	# explain what this test is for
@@ -40,7 +47,7 @@ def resolution_real():
 	# work out what the initial conditions should be
 	p = ct_phantom(material.name, 256, 2)
 	s = source.photon('80kVp, 1mm Al')
-	y = scan_and_reconstruct(s, material, p, 0.01, 256)
+	y = scan_and_reconstruct(s, material, p, 0.01, 256, caxis = [0,6])
 
 	# save some meaningful results
 	save_plot(p[127,:], 'results', 'resolution_real_phantom_127')
@@ -53,6 +60,19 @@ def resolution_real():
 	# how to check whether these results are actually correct?
 	#The reconstructed image should show an impulse of certain width (~5 pixels = 0.5mm in this case).
 	#Impulse are still visible on reconstructed image line 128 onwards, which spread out and die down eventually.
+
+# def test_2():
+#     	# returns impulse response of the back projection
+
+# 	# work out what the initial conditions should be
+# 	p = ct_phantom(material.name, 256, 2)
+# 	s = source.photon('80kVp, 1mm Al')
+# 	y = scan_and_reconstruct(s, material, p, 0.01, 256)
+
+# 	# save some meaningful results
+# 	save_plot(y[128,:], 'results', 'test_2_plot')
+
+# 	# Expecting a sharp pulse
 
 def resolution_ideal():
 	# explain what this test is for
@@ -79,7 +99,8 @@ def resolution_ideal():
 	#Impulse are still visible on reconstructed image line 128 onwards, which spread out and die down eventually.
 
 def test_3():
-	# explain what this test is for
+	# This test calculates the mean attenuation of a section of the  reconstruced image
+	# that is dominantly soft tissue. 
 
 	# work out what the initial conditions should be
 	p = ct_phantom(material.name, 256, 1)
@@ -91,12 +112,15 @@ def test_3():
 	f.write('Mean value is ' + str(np.mean(y[64:192, 64:192])))
 	f.close()
 
-	# how to check whether these results are actually correct?
+	# The mean attenuation value should be around 0.179458cm^-1
+	# which is the attenuation coefficient of soft tissue at 0.1MeV
 
 
 # Run the various tests
-#print('Test 1')
-#test_1()
+print('Test 1')
+test_1()
+# print('Test 2')
+# test_2()
 #print('resolution_real')
 #resolution_real()
 print('resolution_ideal')
